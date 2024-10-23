4b67d29034cbdd150c357effc0801ab9cb4c0b1e
etiennjf <jean-frederic.etienne@iohk.io>
2024-10-23 18:45:41 +0200

    solver optimization: removing typo in test cases NormRecFun_15 + add new test case

--- Solver/Tests/Optimize/OptimizeRecFun.lean
+++ Solver/Tests/Optimize/OptimizeRecFun.lean
@@ -116,8 +116,8 @@ def powerNat (a : Nat) (n : Nat) : Nat :=
 -- a recursive function is referenced more than once (i.e., proper use of instance cache)
 #testOptimize ["NormRecFun_13"] ∀ (x y : Nat), powerNat y x + Nat.pow x y = Nat.pow y x + powerNat x y ===> True
 
--- ∀ (x y : Nat), if x < y then powerNat y x else powerNat x y > Nat.pow x y ===>
--- ∀ (x y : Nat), if x < y then Nat.pow y x else Nat.pow x y > Nat.pow x y
+-- ∀ (x y : Nat), if x < y then powerNat y x else powerNat x y < Nat.pow x y ===>
+-- ∀ (x y : Nat), if x < y then Nat.pow y x else Nat.pow x y < Nat.pow x y
 -- NOTE: Equivalence detection between nested opaque function (i.e., here 3 nested level)
 -- NOTE: Also ensures that structural equivalence is properly performed when
 -- a recursive function is referenced more than once (i.e., proper use of instance cache)
@@ -136,6 +136,16 @@ def eqNat (a : Nat) (b : Nat) : Bool :=
 -- NOTE: Also verify effect of normalization performed on Nat.beq in recursive definition,
 -- i.e., `Nat.beq x y ===> x == y`
 #testOptimize ["NormRecFun_15"] ∀ (x y : Nat), Nat.beq x y = eqNat x y ===> True
+
+
+-- ∀ (c : Bool) (x y : Nat), if c then Nat.beq x y else eqNat y x ===>
+-- ∀ (x y : Nat), x = y
+-- NOTE: Equivalence detection with opaque function
+-- NOTE: Also verify effect of normalization performed on Nat.beq in recursive definition,
+-- i.e., `Nat.beq x y ===> x == y`
+-- NOTE: `true = (x == y) ===> x = y`
+#testOptimize ["NormRecFun_16"] ∀ (c : Bool) (x y : Nat), if c then Nat.beq x y else eqNat y x ===>
+                                ∀ (x y : Nat), x = y
 
 
 /-! Test cases to validate when match expressions and recursive functions are NOT wrongly
