11645d399bccd6233e753517929fa521577662ad
etiennjf <jean-frederic.etienne@iohk.io>
2024-12-16 01:49:20 +0100

    smt translation: update SmtNat.lean to include Nat div and mod test cases

--- Solver/Tests/Smt/SmtNat.lean
+++ Solver/Tests/Smt/SmtNat.lean
@@ -1,5 +1,7 @@
 
 import Solver.Tests.Smt.SmtNat.SmtNatAdd
+import Solver.Tests.Smt.SmtNat.SmtNatDiv
+import Solver.Tests.Smt.SmtNat.SmtNatMod
 import Solver.Tests.Smt.SmtNat.SmtNatMul
 import Solver.Tests.Smt.SmtNat.SmtNatPow
 import Solver.Tests.Smt.SmtNat.SmtNatSub
