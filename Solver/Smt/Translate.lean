b5bf2d82e39c0147ccef07d18f787895a5ddf8f3
etiennjf <jean-frederic.etienne@iohk.io>
2025-01-10 18:29:47 +0100

    smt translation: bugfix type abbrevation in inductive datatype definition

--- Solver/Smt/Translate.lean
+++ Solver/Smt/Translate.lean
@@ -12,6 +12,7 @@ namespace Solver.Smt
 partial def translateExpr (e : Expr) : TranslateEnvT Unit := do
   let rec visit (e : Expr) (topLevel := false) : TranslateEnvT SmtTerm := do
     withTranslateEnvCache e fun _ => do
+    trace[Translate.expr] f!"translating {reprStr e}"
     logReprExpr "Translate:" e
     if let some n := isIntValue? e then return intLitSmt n
     if let some n := isNatValue? e then return natLitSmt n
@@ -23,6 +24,7 @@ partial def translateExpr (e : Expr) : TranslateEnvT Unit := do
      | Expr.forallE .. =>
          let qtyEnv := initialQuantifierEnv topLevel
          let (t, _) ← translateForAll e optimizeExpr visit |>.run qtyEnv
+         trace[Translate.forAll] f!"translate forall {reprStr e} ==> {t}"
          return t
      | Expr.app .. => translateApp e optimizeExpr visit
      | Expr.lam .. => translateLambda e optimizeExpr visit
@@ -31,13 +33,13 @@ partial def translateExpr (e : Expr) : TranslateEnvT Unit := do
         | none => visit me
         | some (Expr.app (Expr.const s _) _) =>
             return mkSimpleSmtAppN (nameToSmtSymbol s) #[smtSimpleVarId (mkReservedSymbol "@x")]
-        | some s => throwEnvError f!"unexpected ctor selector expression {reprStr s}"
+        | some s => throwEnvError f!"translateExpr: unexpected ctor selector expression {reprStr s}"
      | Expr.proj n idx p => translateProj n idx p visit
-     | Expr.lit .. => throwEnvError f!"unexpected literal expression {e}"
-     | Expr.mvar .. => throwEnvError f!"unexpected meta variable {e}"
-     | Expr.bvar .. => throwEnvError f!"unexpected bounded variable {e}"
-     | Expr.letE .. => throwEnvError f!"unexpected let expression"
-     | Expr.sort _ => throwEnvError f!"unexpected sort type {reprStr e}" -- sort type are handled elsewhere
+     | Expr.lit .. => throwEnvError f!"translateExpr: unexpected literal expression {reprStr e}"
+     | Expr.mvar .. => throwEnvError f!"translateExpr: unexpected meta variable {reprStr e}"
+     | Expr.bvar .. => throwEnvError f!"translateExpr: unexpected bounded variable {reprStr e}"
+     | Expr.letE .. => throwEnvError f!"translateExpr: unexpected let expression {reprStr e}"
+     | Expr.sort _ => throwEnvError f!"translateExpr: unexpected sort type {reprStr e}" -- sort type are handled elsewhere
   let st ← visit e (topLevel := true)
   -- assert negation for check sat
   assertTerm (notSmt st)
@@ -65,5 +67,9 @@ def translate (sOpts: SolverOptions) (stx : Syntax) : TermElabM Unit := do
     if Expr.isProp e_type then return e
     if Expr.isProp forall_type then return e_type -- case when parsed term is a theorem name
     throwError f!"translate: Term of type Prop expected !!!"
+
+initialize
+   registerTraceClass `Translate.expr
+   registerTraceClass `Translate.forAll
 
 namespace Solver.Smt
