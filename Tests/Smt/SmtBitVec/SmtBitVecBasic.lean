33600bfa2cb2dcd5c2f5b6931fc26f34a0f25c3c
RSoulatIOHK <romain.soulat@iohk.io>
2026-02-24 23:04:01 +0100

    Changed tests to only-optimize

--- Tests/Smt/SmtBitVec/SmtBitVecBasic.lean
+++ Tests/Smt/SmtBitVec/SmtBitVecBasic.lean
@@ -14,39 +14,39 @@ namespace Test.SmtBitVecBasic
 
 #solve [∀ (x y z : BitVec 8), (x + y) + z = x + (y + z)]
 
-#solve [∀ (x : BitVec 8), x + 0 = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x + 0 = x]
 
-#solve [∀ (x : BitVec 8), 0 + x = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), 0 + x = x]
 
 /-! # Test cases to validate `BitVec.sub` semantics -/
 
-#solve [∀ (x : BitVec 8), x - 0 = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x - 0 = x]
 
-#solve [∀ (x : BitVec 8), x - x = 0]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x - x = 0]
 
 /-! # Test cases to validate `BitVec.mul` semantics -/
 
-#solve [∀ (x y : BitVec 8), x * y = y * x]
+#solve (only-optimize: 1) [∀ (x y : BitVec 8), x * y = y * x]
 
-#solve [∀ (x : BitVec 8), x * 1 = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x * 1 = x]
 
-#solve [∀ (x : BitVec 8), x * 0 = 0]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x * 0 = 0]
 
 /-! # Test cases to validate bitwise operations -/
 
-#solve [∀ (x : BitVec 8), x &&& x = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x &&& x = x]
 
-#solve [∀ (x : BitVec 8), x ||| x = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x ||| x = x]
 
-#solve [∀ (x : BitVec 8), x ^^^ x = 0]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x ^^^ x = 0]
 
-#solve [∀ (x : BitVec 8), x &&& 0 = 0]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x &&& 0 = 0]
 
-#solve [∀ (x : BitVec 8), x ||| 0 = x]
+#solve (only-optimize: 1) [∀ (x : BitVec 8), x ||| 0 = x]
 
 /-! # Test cases to validate `BitVec` comparisons -/
 
-#solve [∀ (x y : BitVec 8), BitVec.ult x y ↔ ¬ BitVec.ule y x]
+#solve (only-optimize: 1) [∀ (x y : BitVec 8), BitVec.ult x y ↔ ¬ BitVec.ule y x]
 
 /-! # Test cases to ensure that counterexamples are properly detected -/
 
@@ -54,4 +54,5 @@ namespace Test.SmtBitVecBasic
 
 #solve (gen-cex: 0) (solve-result: 1) [∀ (x : BitVec 8), x ≠ x]
 
+#solve (solve-result: 1) (gen-cex: 0) [∀ (x y : BitVec (4 + 3)), x + y = y - x]
 end Test.SmtBitVecBasic
