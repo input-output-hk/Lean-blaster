844622b7be5a87f754ec8ce2fecfe35f87d95fad
etiennejf <jean-frederic.etienne@iohk.io>
2026-02-28 13:27:17 +0100

    Test suite: add random-seed to issue16 to issue18 to consider extensionality patch

--- Tests/FixedIssues/Issue16.lean
+++ Tests/FixedIssues/Issue16.lean
@@ -26,12 +26,12 @@ end
          (∀  (x y : Int) (f : Int → Nat), f x + f y > 20)
        ]
 
-#blaster (gen-cex:0) (solve-result: 1)
+#blaster (gen-cex:0) (solve-result: 1) (random-seed: 1)
        [ (∀ (β : Type) (x : Term (List β)) (f : Term (List β) → Nat), f x > 0) →
          (∀ (α : Type) (x y : Term (List α)) (f : Term (List α) → Nat), f x + f y > 20)
        ]
 
-#blaster (gen-cex:0) (solve-result: 1) (random-seed: 2)
+#blaster (gen-cex:0) (solve-result: 1) (random-seed: 1)
        [ (∀ (β : Type) (x : Term (List β)) (g : Term (List β) → Nat), g x > 10) →
          (∀ (α : Type) (x y : Term (List α)) (f : Term (List α) → Nat), f x + f y > 30)
        ]
