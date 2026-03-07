4c8b5c44d0f70c3190a9fa460c8e11c3ec95074c
Felipe Péret <40478095+felipeperet@users.noreply.github.com>
2026-03-07 17:14:28 -0300

    Removing tactic script approach

--- Blaster/Reconstruct.lean
+++ Blaster/Reconstruct.lean
@@ -1,3 +1,2 @@
 import Blaster.Reconstruct.Trace
-import Blaster.Reconstruct.Tactic
 import Blaster.Reconstruct.Basic
--- Tests/Reconstruct/Basic.lean
+++ Tests/Reconstruct/Basic.lean
@@ -1,12 +1,1 @@
-import Blaster.Reconstruct
 
-open Blaster.Reconstruct
-
-example (x : Nat) : x + 0 = x := by
-  reconstruct [.Rewrite `Nat.add_zero]
-
-example (x : Nat) : 0 + x = x := by
-  reconstruct [.Rewrite `Nat.zero_add]
-
-example (x : Nat) : 0 + x + 0 = x := by
-  reconstruct [.Rewrite `Nat.zero_add, .Rewrite `Nat.add_zero]
