2a371957e2c0b91ebdb8185c1aa65a5ecb7009c4
RSoulatIOHK <romain.soulat@iohk.io>
2026-06-08 12:17:11 +0200

    test: Addition commutativity/associativity/identity/negation checks

--- Tests/Ratio/Addition.lean
+++ Tests/Ratio/Addition.lean
@@ -36,4 +36,61 @@ namespace Tests.Ratio.Addition
 -- ADD_CONSTANTS_5: 85/100 + -15/-100 = 1
 #blaster [ (eqRatio (addRatio (ratio 85 100) (ratio (-15) (-100))) R_ONE) = true ]
 
+/- AdditionCommutativity -/
+
+-- ADD_COMMUTATIVITY
+#blaster [ ∀ (a_n a_d b_n b_d : Int),
+  let a := ratio a_n a_d
+  let b := ratio b_n b_d
+  isValidRatio a = true → isValidRatio b = true → b.isNaN = false →
+  (addRatio a b == addRatio b a) = true ]
+
+/- AdditionAssociativity -/
+
+-- ADD_ASSOCIATIVITY_1: (a + b) + c = a + (b + c)
+#blaster [ ∀ (a_n a_d b_n b_d c_n c_d : Int),
+  let a := ratio a_n a_d
+  let b := ratio b_n b_d
+  let c := ratio c_n c_d
+  isValidRatio a = true → isValidRatio b = true →
+  (addRatio (addRatio a b) c == addRatio a (addRatio b c)) = true ]
+
+-- ADD_ASSOCIATIVITY_2: (a + c) + b = (a + b) + c
+#blaster [ ∀ (a_n a_d b_n b_d c_n c_d : Int),
+  let a := ratio a_n a_d
+  let b := ratio b_n b_d
+  let c := ratio c_n c_d
+  isValidRatio a = true → isValidRatio b = true →
+  (addRatio (addRatio a c) b == addRatio (addRatio a b) c) = true ]
+
+/- AdditionIdentity -/
+
+-- IDENTITY_LEFT: 0 + a = a
+#blaster [ ∀ (a_n a_d : Int),
+  let a := ratio a_n a_d
+  isValidRatio a = true →
+  ((addRatio R_ZERO a == a) && eqRatio (addRatio R_ZERO a) a) = true ]
+
+-- IDENTITY_RIGHT: a + 0 = a
+#blaster [ ∀ (a_n a_d : Int),
+  let a := ratio a_n a_d
+  isValidRatio a = true →
+  ((addRatio a R_ZERO == a) && eqRatio (addRatio a R_ZERO) a) = true ]
+
+/- AdditionNegation -/
+
+-- ADD_OPPOSITE: a + (-a) = 0
+#blaster [ ∀ (a_n a_d b_n b_d : Int),
+  let a := ratio a_n a_d
+  let b := ratio b_n b_d
+  isValidRatio a = true → isValidRatio b = true →
+  (eqRatio (addRatio a (negate a)) R_ZERO) = true ]
+
+-- ADD_NEG_DISTRIB: -(a + b) = -a + -b
+#blaster [ ∀ (a_n a_d b_n b_d : Int),
+  let a := ratio a_n a_d
+  let b := ratio b_n b_d
+  isValidRatio a = true → isValidRatio b = true →
+  (negate (addRatio a b) == addRatio (negate a) (negate b)) = true ]
+
 end Tests.Ratio.Addition
